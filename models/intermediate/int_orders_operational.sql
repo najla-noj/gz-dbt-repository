e5b429f5f7a08ffe876ff5a41e5402dc3f6ecdd0
Najla <najla.albredy@gmail.com>
2024-03-05 15:00:47 +0000

    save

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,15 +1,15 @@
 SELECT
 o.orders_id
 ,o.date_date
-, ROUND (s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin
+,ROUND(s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin
 ,o.quantity
 .o.revenue
 ,o.purchase_cost
-,.margin
+,o.margin
 ,s.shipping_fee
 ,s.logcost
 ,s.ship_cost
-FROM {{ref(" int_orders_margin")}} o
+FROM {{ref("int_orders_margin")}} o
 LEFT JOIN {{ref("stg_raw__ship")}} s
-USING(orders id)
+USING(orders_id)
 ORDER BY orders_id desc
