4502542a1792f6d3fa3a8dbef0a71e59d3eeb277
najla albredy <najla.albredy@gmail.com>
2024-03-06 21:59:29 +0000

    m

---
+++ models/staging/raw/stg_raw__adwords.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'adwords') }}
+
+),
+
+renamed as (
+
+    select
+         date_date ,
+    paid_source ,
+    campaign_key,
+    impression,
+    click,
+    cast(ads_cost AS FLOAT64) AS ads_cost ,
+    camPGN_name aS campaing_name 
+    from source
+
+)
+
+select * from renamed
---
+++ models/staging/raw/stg_raw__criteo.sql
@@ -0,0 +1,25 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'criteo') }}
+
+),
+
+renamed as (
+
+    select
+      
+    date_date ,
+    paid_source ,
+    campaign_key,
+    impression,
+    click,
+    cast(ads_cost AS FLOAT64) AS ads_cost ,
+    camPGN_name aS campaing_name 
+
+    from source
+
+)
+
+select * from renamed
