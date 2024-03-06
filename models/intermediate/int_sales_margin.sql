5248acfc164a71c3b6ec5f7b3860c19e49f6bb58
najla albredy <najla.albredy@gmail.com>
2024-03-06 17:38:31 +0000

    s

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -8,5 +8,5 @@ SELECT
      ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
      s.revenue - ROUND(s.quantity*CAST(purchase_price AS FLOAT64),2) AS margin 
 FROM {{ref("stg_raw__sales")}} s 
-LEFT JOUN {{ref("stg_raw__product")}} p 
+LEFT JOIN {{ref("stg_raw__product")}} p 
     USING (products_id)
